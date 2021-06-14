889c2ac20157f1f7c0bc8def28aeeb96fb4e07d7
Garlinsk <franklinwanami@gmail.com>
2021-06-14 05:48:30 +0300

    add  modified javascript/script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,35 @@
-var male = {"Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
-var female ={Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
-var akanName ={}
+var male = (DayOfTheWeek){"Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
+var female =(DayOfTheWeek){Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
+var akanName =(male){
+    Sunday: Kwasi
+
+    Monday: Kwadwo
+    
+    Tuesday: Kwabena
+    
+    Wednesday: Kwaku
+    
+    Thursday:  Yaw
+    
+    Friday: Kofi
+    
+    Saturday: Kwame
+    
+    }
+var akanName =(female){
+    Sunday: Akosua
+
+Monday: Adwoa
+
+Tuesday: Abenaa
+
+Wednesday: Akua
+
+Thursday:  Yaa
+
+Friday: Afua
+
+Saturday: Ama
+
+}
+DayOfTheWeek (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
