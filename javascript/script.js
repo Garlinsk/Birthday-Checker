5c16762bf67e1c043a857597d2287735b5ef0a85
Garlinsk <franklinwanami@gmail.com>
2021-06-14 13:44:33 +0300

    add  modified css/bootstrap.css.map,modified css/style.css,index.html,and modified javascript/script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,53 @@
-var male =DayOfTheWeek{"Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
-var female =DayOfTheWeek{Sunday","Monday","Tuesday" ,"Wenesday","Thursday,"Friday","Saturday"}
-let male =akanName{
-    Sunday: Kwasi
 
-    Monday: Kwadwo
-    
-    Tuesday: Kwabena
-    
-    Wednesday: Kwaku
-    
-    Thursday:  Yaw
-    
-    Friday: Kofi
-    
-    Saturday: Kwame
-    console.log(akanName)
-    
+function akanName () {
+    let yearOfBirth = document.getElementById("year").value;
+    let monthOfBirth = document.getElementById("month").value;
+    let dayOfBirth = document.getElementById("day").value;
+    let gender = document.getElementById("gender").value;
+
+    //validating inputs
+    if (yearOfBirth.length !== 4){
+        alert("invalid year")
     }
-if female =akanName{
-    Sunday: Akosua
 
-Monday: Adwoa
+    if (parseInt(monthOfBirth) < 1 || parseInt(monthOfBirth) > 12 ){
+        alert("invalid month")
+    }
 
-Tuesday: Abenaa
+    if (parseInt(dayOfBirth) < 1 || parseInt(dayOfBirth) > 31 ){
+        alert("invalid date")
+    }
 
-Wednesday: Akua
+    let CC = parseInt(yearOfBirth.substr(0,2));
+    let YY = parseInt(yearOfBirth.substr(2,));
+    let MM = parseInt(monthOfBirth);
+    let DD = parseInt(dayOfBirth);
 
-Thursday:  Yaa
 
-Friday: Afua
+    let d = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7);
 
-Saturday: Ama
-console.log(akanName)
+    //Arrays
+    let daysOfWeek = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
+    let akanMales = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame" ];
+    
+    let akanFemales = [ "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+
+    let dayOfWeek = daysOfWeek[d];
+
+    //Calculate gender
+    let name;
+    if (gender === "male") {
+    name = akanMales[d];
+    }else if (gender === "female") {
+    name = akanFemales[d];
+    }else {
+    alert("Please select Gender")
+    }
+
+    //let now display to the html
+    document.getElementById("daysOfTheWeek").innerHTML = daysOfTheWeek[d]
+    document.getElementById("akanName").innerHTML = name;
+
+
 }
-let DayOfTheWeek (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-console.log(let DayOfTheWeek (d))
